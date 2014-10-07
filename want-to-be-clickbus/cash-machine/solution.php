841d740ab9d48107f06c1f87cc3b35c3e0126096
Victor Szelagowski <wencessz@gmail.com>
2014-10-07 09:01:35 -0300

    Update solution.php

--- want-to-be-clickbus/cash-machine/solution.php
+++ want-to-be-clickbus/cash-machine/solution.php
@@ -9,33 +9,33 @@ Class CashMachine{
     $this->availableMoney = $values;
   }
   Private function checkIsValid($money,$value){
-	$mod = $money % $value;
+    $mod = $money % $value;
     return($mod != $money);
   }
   Public function getMoney($money=null){
     $return = array();
-	if(!isset($money)){
-	  throw new InvalidArgumentException('[Empty Set]');
-	}
-	if($money < 0){
-	  throw new InvalidArgumentException('getMoney function only accepts integers positives. Input was: '.$money);
-	}
-	$valuesleft = $money;
-	while($valuesleft > 0){ //while the money still left
-	  $prevalue = $valuesleft;
-	  foreach($this->availableMoney as $key => $value){ //loop around the money in the machine
-		if($this->checkIsValid($valuesleft,$value)){ //if valueleft - value is still valid
-		  $valuesleft =  $valuesleft - $value;
-	      $return[] = $value;
-		  break;
-		}
-	  }
-	  if($valuesleft == $prevalue){ //if we checked all values and anything has changed, its mean that is not a valid number
-	    throw new InvalidArgumentException('Onpy accepts partible money. Input was: '.$money);
-	  }
-	}
-	return $return;
-  }
+    if(!isset($money)){
+      throw new InvalidArgumentException('[Empty Set]');
+    }
+    if($money < 0){
+      throw new InvalidArgumentException('getMoney function only accepts integers positives. Input was: '.$money);
+    }
+    $valuesleft = $money;
+    while($valuesleft > 0){ //while the money still left
+      $prevalue = $valuesleft;
+      foreach($this->availableMoney as $key => $value){ //loop around the money values in the machine
+   	if($this->checkIsValid($valuesleft,$value)){ //if value is still valid
+          $valuesleft =  $valuesleft - $value;
+          $return[] = $value;
+          break;
+        }
+      }
+      if($valuesleft == $prevalue){ //if we check all values and anything has changed, this mean that is not a valid number
+        throw new InvalidArgumentException('Onpy accepts partible money. Input was: '.$money);
+      }
+    }
+    return $return;
+   }
 }
 
 $machine = new CashMachine(array(100,20,50,10,5,3));
